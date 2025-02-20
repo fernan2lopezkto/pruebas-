40b0d91ead2ad240e6286acb35aec6631d4770d9
Juan <juanPc>
2025-02-19 21:11:12 -0300

    ver dulce gracia next static

--- README.md
+++
@@ -1,3 +0,0 @@
-# link de la página 
-
-<a href="https://fernan2lopezkto.github.io/pruebas-">Pruebas-</a>
--- service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker instalado');
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => new Response('Sin conexión a Internet'))
-  );
-});
